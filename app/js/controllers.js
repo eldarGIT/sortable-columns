2caf5ea07b56813ce779ba221440fcd5e0fc73c8
Eldar <eldar.ablaev@gmail.com>
2014-02-17 12:52:29 +0200

    realize simple sortable columns

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
+angular.module('controllers', []).
+  controller('appCtrl', ['$scope', function($scope) {
+		$scope.rawScreens = [
+			[{title: "Title 11", link: "link1"}, {title: "Title 12", link: "link2"}],
+			[{title: "Title 21", link: "link1"}, {title: "Title 22", link: "link2"}],
+		];
+		$scope.sortableOptions = {
+			placeholder: "app",
+			connectWith: ".apps-container"
+		};
   }]);
